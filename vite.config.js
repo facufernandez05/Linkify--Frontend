4862630a1a06e86dcfd107b502728a98fbccee06
Facundo Fernandez <facuufdez05@gmail.com>
2023-10-25 23:23:09 -0300

    change base config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Linkify--Frontend/'
+  base: 'http://facufernandez05.github.io/Linkify--Frontend'
 })
